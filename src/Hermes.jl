362e2a284e721a796aaacea7b5c635e8a39c6cc8
Stefan Bringuier <stefanbringuier@gmail.com>
2023-07-21 15:10:26 -0700

    Moved ui into api.

--- src/Hermes.jl
+++ src/Hermes.jl
@@ -2,13 +2,13 @@ module Hermes
 using Reexport
 
 include("instruct.jl")
-include("ui.jl")
+#include("ui.jl")
 include("api.jl")
 include("display.jl")
 include("utils.jl")
 
-global OPENAI = (apikey="",model="")
-@reexport using .ui
+#global OPENAI = (apikey="",model="")
+#@reexport using .ui
 @reexport using .api
 
 end
