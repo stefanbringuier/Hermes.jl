0b706ad0d87f5254a689b3914462228a053bae37
Stefan Bringuier <stefanbringuier@gmail.com>
2023-07-21 16:30:05 -0700

    Fix.

--- src/ui.jl
+++ src/ui.jl
@@ -12,7 +12,7 @@ const MODELS = ["gpt-3.5-turbo","gpt-3.5-turbo-16k","gpt-4","gpt-4-32k"]
 
 global OPENAI = (apikey="", model="")
 
-macro set_openai(apikey::String,model::String)
+macro set_openai(apikey,model)
     return eval(:(global OPENAI = (apikey=$(apikey),model=$(model))))
 end
 
